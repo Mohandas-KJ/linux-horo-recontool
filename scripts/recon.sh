d5ba75693f75ceb8df0898b3006713a60e1e2bbd
Mohan Das <mohandaskj.11@gmail.com>
2025-12-04 19:24:30 +0530

    The Script could give some information about the CPU

--- scripts/recon.sh
+++ scripts/recon.sh
@@ -11,3 +11,11 @@ echo "[OS]"
 echo "OS Name: $(grep "^PRETTY_NAME" /etc/os-release | cut -d= -f2 | tr -d "\"")"
 echo "Kernel Name: $(uname -r)"
 echo "Kernel Version: $(uname -r | cut -d- -f1)"
+echo 
+
+echo "[CPU]"
+echo "CPU Model: $(lscpu | grep "Model name:" | tr -s ' ' | cut -d: -f2)"
+echo "Architecture: $(lscpu | grep "Architecture:" | tr -s ' ' | cut -d: -f2)"
+echo "Operation Modes: $(lscpu | grep "CPU op-mode(s):" | tr -s ' ' | cut -d: -f2)"
+echo "Byte Order: $(lscpu | grep "Byte Order:" | tr -s ' ' | cut -d: -f2)"
+echo "Number of Sockets: $(lscpu | grep "Socket(s):" | tr -s ' ' | cut -d: -f2)"
