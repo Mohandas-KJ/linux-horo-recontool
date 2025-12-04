111ce03e6d3ae0da05cbcd0776b859cbe35edef8
DECODER LINUX <decoder_linux@dedsec.home.io>
2025-12-04 19:02:04 +0530

    The Script could print OS details

--- scripts/recon.sh
+++ scripts/recon.sh
@@ -5,3 +5,9 @@ echo "=================================="
 echo "[User and System]"
 echo "User: $USER"
 echo "HostName: $(hostname)"
+echo 
+
+echo "[OS]"
+echo "OS Name: $(grep "^PRETTY_NAME" /etc/os-release | cut -d= -f2 | tr -d "\"")"
+echo "Kernel Name: $(uname -r)"
+echo "Kernel Version: $(uname -r | cut -d- -f1)"
